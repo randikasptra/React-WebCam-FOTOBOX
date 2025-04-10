3d16a0aeeba8371e2bb985e4f32aecd1f05c634d
randikasptra <saputrarandika857@gmail.com>
2025-04-10 07:40:36 +0700

    Memperbaiki nama

---
+++ src/page/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Homepage from '../components/HomePages'
+
+const home = () => {
+    return (
+        <Homepage />
+    )
+}
+
+export default home
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { BrowserRouter as Router , Route, Routes } from 'react-router-dom'
+import Home from '../page/home'
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
